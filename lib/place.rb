0ee8a65f429d09acd08482379790dc84bba87fcb
Adam Deakin <adeakin89@gmail.com>
2022-04-05 15:32:23 +0100

    Added attr reader to place.rb

--- lib/place.rb
+++ lib/place.rb
@@ -1,5 +1,15 @@
-require 'database_connection'
+require './lib/database_connection'
 
 class Place
-
+  attr_reader :id, :host_name, :host_email, :place_title, :place_price, :location, :description
+  def initialize(id:, host_name:, host_email:, place_title:, place_price:, location:, description:)
+    @id = id
+    @host_name = host_name
+    @host_email = host_email
+    @place_title = place_title
+    @place_price = place_price
+    @location = location
+    @description = description
+  end
+  
 end
