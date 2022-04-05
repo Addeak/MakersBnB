648efcb2bdacf5c5660cde1b432607a9e8c8b6cb
Adam Deakin <adeakin89@gmail.com>
2022-04-05 14:40:30 +0100

    Added views and controller for requesting feature

--- app.rb
+++ app.rb
@@ -16,5 +16,22 @@ class MakersBnB < Sinatra::Base
     'Testing testing'
   end
 
+  get '/places/request' do
+    @place = Place.new(id: params[:id])
+    erb :'places/request'
+  end
+
+  post '/places/confirmation' do
+    @customer = params[:customer_name]
+    Request.send_email(
+      customer_name: params[:customer_name],
+      customer_email: params[:customer_email],
+      check_in: params[:check_in],
+      check_out: params[:check_out],
+      comments: params[:comments_box]
+    )
+    erb :'places/confirmation'
+  end
+
   run! if app_file == $0
 end
