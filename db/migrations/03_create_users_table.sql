76abb2f22ce53e63108b803b3edaa9b4d85cb34f
asiminaathanasopoulou <Asimina.Ath@gmail.com>
2022-04-06 15:52:00 +0100

    file name update

--- db/migrations/03_create_users_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY, 
-  user_first_name VARCHAR(60), 
-  user_surname VARCHAR(60),
-  user_email VARCHAR(255), 
-  user_password VARCHAR(140),
-  mobile_number VARCHAR(15)
-);
