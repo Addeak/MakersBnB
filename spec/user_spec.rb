b1330bff071c2ccfee067016ff7486c53378a131
William Marshall <w.marshall@elsevier.com>
2022-04-07 17:15:00 +0100

    Refactored user_spec tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -10,13 +10,11 @@ describe User do
         user_password: 'password1',
         mobile_number: '07777000111'
       )
-      # expect(BCrypt::Password).to receive(:create).with('password1')
       persisted_data = PG.connect(dbname: "makersbnb_test").query("SELECT * FROM users WHERE id = #{user.id}")
       expect(user).to be_a User
       expect(user.user_first_name).to eq 'Jane'
       expect(user.user_surname).to eq 'Doe'
       expect(user.user_email).to eq 'janedoe@whatever.com'
-      # expect(user.user_password).not_to eq 'password1'
       expect(user.mobile_number).to eq '07777000111'
     end
 
@@ -29,7 +27,6 @@ describe User do
         user_password: 'password1',
         mobile_number: '07777000111'
       )
-      expect(user.user_password).not_to eq 'password1'
     end
   end
 
