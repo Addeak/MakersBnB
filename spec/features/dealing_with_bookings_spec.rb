ddcb91e8e7a1a7f948bba709843a2a95c37a90da
Jon Clarke <jonclarke1984@googlemail.com>
2022-04-08 15:21:57 +0100

    updated nav bars while writing host list tests

--- spec/features/dealing_with_bookings_spec.rb
+++ spec/features/dealing_with_bookings_spec.rb
@@ -1,28 +1,34 @@
-xfeature 'Dealing with bookings' do
+feature 'Dealing with bookings' do
   scenario 'A customer user with no requests, can see an empty list of requests' do
     register_user
-    click_button 'My Requests'
+    visit '/users/myrequests'
 
     expect(page).to have_content 'You have no bookings'
   end
 
   scenario 'A host can accept a requested booking' do
-    register_user
-    add_place
+    register_user # first_name: 'Jon', surname: 'Doe', email: 'jondoe@example.org'
+    add_place # 'Test Place'
+    click_button 'Log-out'
     register_user(first_name: 'Michael', surname: 'Jordan', email: 'mjordan@example.org')
-
+    
     click_button 'Request Stay'
     
     fill_in 'check_in', with: '2022-4-4'
     fill_in 'check_out', with: '2022-4-7'
     click_button 'Submit'
 
+    click_button 'Log-out'
+
     visit '/places/list'
     fill_in 'user_email', with: 'mjordan@example.org'
     fill_in 'user_password', with: 'password'
-    click_button 'My Requests'
-
-    expect(page).to have_content ????????????
+    click_button 'Log-in'
+    visit '/users/myrequests'
+   
+    expect(page).to have_content 'Test Place'
     expect(page).to have_content 'Status: Pending'
+    # expect(page).to have_button 'Accept'
+    # expect(page).to have_button 'Decline'
   end
 end
